b715dfef2e19af570bdfff04f9770066d3dcffbd
Jackeline Aguas <20193tn001@utez.edu.mx>
2022-10-01 18:05:26 -0500

    add design activities of login, rate and service

---
+++ app/src/main/java/com/pandadevs/heyfix_client/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.pandadevs.heyfix_client
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pandadevs.heyfix_client.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
