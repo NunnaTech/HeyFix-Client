d8d30c6f423bc9653b4047be84f52fc4eb0dbcd2
RaulPC <sterplay2010@yahoo.com>
2022-09-30 19:56:17 -0500

    feat(frontend): finish home and service views

--- app/src/main/java/com/pandadevs/heyfix_client/RequestServiceActivity.kt
+++ app/src/main/java/com/pandadevs/heyfix_client/RequestServiceActivity.kt
@@ -2,6 +2,7 @@ package com.pandadevs.heyfix_client
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.pandadevs.heyfix_client.databinding.ActivityRequestServiceBinding
 
 class RequestServiceActivity : AppCompatActivity() {
@@ -10,5 +11,13 @@ class RequestServiceActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityRequestServiceBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.chat.setOnClickListener {
+            Toast.makeText(this, "Si funciona este boton de chat", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.cancelar.setOnClickListener {
+            Toast.makeText(this, "Si funciona este boton de cancelar", Toast.LENGTH_SHORT).show()
+        }
     }
 }
